27afd54e43b61992786e21c8d09ff128abd2b9e9
Nan Sha <wytsn@163.com>
2011-06-16 04:31:43 +0800

    app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koalablog::Application.config.secret_token = 'eb9681e08a979dee2e2de55dc2be1dcd94c86133537dd376be677792015177efea9ea190fd0d8b30022ec76b8cadccdddae9efd34239233511a3ca7ee00e7929'
